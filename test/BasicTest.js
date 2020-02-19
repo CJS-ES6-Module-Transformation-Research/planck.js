5120e8b6ecabca471e15b9f7f4a4e5d47bb8ee34
katerina <katpaltoglou@gmail.com>
2020-02-19 18:33:04 +0200

    update branch

--- test/BasicTest.js
+++ test/BasicTest.js
@@ -1,44 +1,53 @@
-import { expect as BasicTest_expect } from "./testutil/expect";
-import sinon from "sinon";
-import { Vec2 as BasicTest_Vec2 } from "../lib/common/Vec2";
-import { CircleShape as BasicTest_Circle } from "../lib/shape/CircleShape";
-import { World as BasicTest_World } from "../lib/World";
+var _expect = require("./testutil/expect");
 
-describe('Basic', function() {
+var _sinon = require("sinon");
 
-  it('World', function() {
+var _sinon2 = _interopRequireDefault(_sinon);
 
-    var world = new World();
+var _Vec = require("../lib/common/Vec2");
 
-    var circle = new Circle(1);
+var _CircleShape = require("../lib/shape/CircleShape");
 
-    var b1 = world.createBody({
-      position : Vec2(0, 0),
-      type : 'dynamic'
+var _World = require("../lib/World");
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+
+describe('Basic', function () {
+
+    it('World', function () {
+
+        var world = new World();
+
+        var circle = new Circle(1);
+
+        var b1 = world.createBody({
+            position: Vec2(0, 0),
+            type: 'dynamic'
+        });
+
+        b1.createFixture(circle);
+
+        expect(b1.getFixtureList().getType()).be('circle');
+        expect(b1.getWorld()).be(world);
+        expect(world.getBodyList()).be(b1);
+
+        b1.applyForceToCenter(Vec2(1, 0), true);
+
+        var b2 = world.createBody({
+            position: Vec2(2, 0),
+            type: 'dynamic'
+        });
+        b2.createFixture(circle);
+        b2.applyForceToCenter(Vec2(-1, 0), true);
+
+        world.step(1 / 20);
+
+        // console.log(b2.getPosition());
+
+        var p = b1.getPosition();
+        expect(p.x).near(0.0);
+        expect(p.y).near(0.0);
     });
-
-    b1.createFixture(circle);
-
-    expect(b1.getFixtureList().getType()).be('circle');
-    expect(b1.getWorld()).be(world);
-    expect(world.getBodyList()).be(b1);
-
-    b1.applyForceToCenter(Vec2(1, 0), true);
-
-    var b2 = world.createBody({
-      position : Vec2(2, 0),
-      type : 'dynamic'
-    });
-    b2.createFixture(circle);
-    b2.applyForceToCenter(Vec2(-1, 0), true);
-
-    world.step(1 / 20);
-
-    // console.log(b2.getPosition());
-
-    var p = b1.getPosition();
-    expect(p.x).near(0.0);
-    expect(p.y).near(0.0);
-  });
-
 });
