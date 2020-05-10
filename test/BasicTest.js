7104b78551f172ec87cd6c82615307b3a164d25b
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-10 17:18:01 +0300

    update branch

--- test/BasicTest.js
+++ test/BasicTest.js
@@ -1,44 +1,53 @@
-import { expect as testutilexpect_expectjs } from "./testutil/expect";
-import ext_sinon_sinon from "sinon";
-import { Vec2 as libcommonVec2_Vec2js } from "../lib/common/Vec2";
-import { CircleShape as libshapeCircleShape_CircleShapejs } from "../lib/shape/CircleShape";
-import { World as libWorld_Worldjs } from "../lib/World";
+"use strict";
 
-describe('Basic', function() {
+var _expect = require("./testutil/expect");
 
-  it('World', function() {
+var _sinon = require("sinon");
 
-    var world = new libWorld_Worldjs();
+var _sinon2 = _interopRequireDefault(_sinon);
 
-    var circle = new libshapeCircleShape_CircleShapejs(1);
+var _Vec = require("../lib/common/Vec2");
 
-    var b1 = world.createBody({
-      position : libcommonVec2_Vec2js(0, 0),
-      type : 'dynamic'
+var _CircleShape = require("../lib/shape/CircleShape");
+
+var _World = require("../lib/World");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+describe('Basic', function () {
+
+    it('World', function () {
+
+        var world = new _World.World();
+
+        var circle = new _CircleShape.CircleShape(1);
+
+        var b1 = world.createBody({
+            position: (0, _Vec.Vec2)(0, 0),
+            type: 'dynamic'
+        });
+
+        b1.createFixture(circle);
+
+        (0, _expect.expect)(b1.getFixtureList().getType()).be('circle');
+        (0, _expect.expect)(b1.getWorld()).be(world);
+        (0, _expect.expect)(world.getBodyList()).be(b1);
+
+        b1.applyForceToCenter((0, _Vec.Vec2)(1, 0), true);
+
+        var b2 = world.createBody({
+            position: (0, _Vec.Vec2)(2, 0),
+            type: 'dynamic'
+        });
+        b2.createFixture(circle);
+        b2.applyForceToCenter((0, _Vec.Vec2)(-1, 0), true);
+
+        world.step(1 / 20);
+
+        // console.log(b2.getPosition());
+
+        var p = b1.getPosition();
+        (0, _expect.expect)(p.x).near(0.0);
+        (0, _expect.expect)(p.y).near(0.0);
     });
-
-    b1.createFixture(circle);
-
-    testutilexpect_expectjs(b1.getFixtureList().getType()).be('circle');
-    testutilexpect_expectjs(b1.getWorld()).be(world);
-    testutilexpect_expectjs(world.getBodyList()).be(b1);
-
-    b1.applyForceToCenter(libcommonVec2_Vec2js(1, 0), true);
-
-    var b2 = world.createBody({
-      position : libcommonVec2_Vec2js(2, 0),
-      type : 'dynamic'
-    });
-    b2.createFixture(circle);
-    b2.applyForceToCenter(libcommonVec2_Vec2js(-1, 0), true);
-
-    world.step(1 / 20);
-
-    // console.log(b2.getPosition());
-
-    var p = b1.getPosition();
-    testutilexpect_expectjs(p.x).near(0.0);
-    testutilexpect_expectjs(p.y).near(0.0);
-  });
-
 });
